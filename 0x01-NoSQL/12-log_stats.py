11244dc90d237be01cb2ff52ad618dbc618d832c
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-18 00:29:13 +0300

    Commit files for the thirteenth task

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ This module defines a script that provides some stats about Nginx logs stored in
 
 from pymongo import MongoClient
 
-
+"""
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
@@ -22,3 +22,26 @@ if __name__ == "__main__":
         {"method": "GET", "path": "/status"}
         )
     print(f'{status_check} status check')
+"""
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017')
+db = client['logs']
+collection = db['nginx']
+
+# Get the total number of documents in the collection
+total_logs = collection.count_documents({})
+
+# Get the number of documents with each HTTP method
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}
+
+# Get the number of documents with method=GET and path=/status
+status_check = collection.count_documents({"method": "GET", "path": "/status"})
+
+# Print the results
+print(f"{total_logs} logs")
+print("Methods:")
+for method, count in method_counts.items():
+    print(f"    method {method}: {count}")
+print(f"{status_check} status check")
